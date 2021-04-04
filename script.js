bb33f798ae4a9b6096d44730d4fe50c0ba36ee45
Wellington dos Santos <wellington1998santoss@gmail.com>
2021-04-04 16:24:24 -0300

    Visualização das transações

--- script.js
+++ script.js
@@ -1,8 +1,127 @@
 const Modal = {
+  // Adiciona a classe active e abre o modal
   open() {
     document.querySelector(".modal-overlay").classList.add("active");
   },
+  // Remove a classe active e fecha o modal
   close() {
     document.querySelector(".modal-overlay").classList.remove("active");
   },
 };
+
+const transactions = [
+  {
+    id: 1,
+    description: "Luz",
+    amount: -50000,
+    date: "23/01/2021",
+  },
+  {
+    id: 2,
+    description: "Criação de Website",
+    amount: 500000,
+    date: "23/01/2021",
+  },
+  {
+    id: 3,
+    description: "Internet",
+    amount: -20000,
+    date: "23/01/2021",
+  },
+  {
+    id: 4,
+    description: "Criação de App",
+    amount: 200000,
+    date: "23/01/2021",
+  },
+];
+
+const Transaction = {
+  all: transactions,
+
+  // Somar entradas
+  incomes() {
+    let income = 0;
+
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount > 0) {
+        income += transaction.amount;
+      }
+    });
+    
+    return income;
+  },
+
+  // Somas saídas
+  expenses() {
+    let expense = 0;
+
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount < 0) {
+        expense += transaction.amount;
+      }
+    });
+
+    return expense;
+  },
+
+  // Balanço de entradas e saídas
+  total() {
+    return Transaction.incomes() + Transaction.expenses();
+  },
+};
+
+const DOM = {
+  transactionsContainer: document.querySelector("#data-table tbody"),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+
+    DOM.transactionsContainer.appendChild(tr);
+  },
+
+  innerHTMLTransaction(transaction) {
+    const cssClass = transaction.amount > 0 ? "income" : "expense";
+
+    const amount = Utils.formatCurrency(transaction.amount);
+
+    const html = `
+      <td class="description">${transaction.description}</td>
+      <td class="${cssClass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover Transação" />
+      </td>
+    `;
+
+    return html;
+  },
+
+  updateBalance() {
+    document.querySelector("#incomeDisplay").innerHTML = Utils.formatCurrency(Transaction.incomes());
+    document.querySelector("#expenseDisplay").innerHTML = Utils.formatCurrency(Transaction.expenses());
+    document.querySelector("#totalDisplay").innerHTML = Utils.formatCurrency(Transaction.total());
+  },
+};
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : "";
+
+    value = String(value).replace(/\D/g, "");
+    value = Number(value) / 100;
+    value = value.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    return signal + " " + value;
+  },
+};
+
+Transaction.all.forEach((transaction) => {
+  DOM.addTransaction(transaction);
+});
+
+DOM.updateBalance();
